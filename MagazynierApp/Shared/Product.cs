647bfcec14f8799bab57b5985bbb20f309694f14
Totokis <pawelj_janusz@wp.pl>
2021-08-25 09:13:56 +0200

    Added highlight for empty machine

--- MagazynierApp/Shared/Product.cs
+++ MagazynierApp/Shared/Product.cs
@@ -8,11 +8,11 @@ namespace MagazynierApp.Shared
         public string ImageUrl { get; set; }
         public int Quantity { get; set; }
         public int MaxQuantity { get; set; }
+        public int MinQuantity { get; set; }
 
-        public bool NeedToRefill => Quantity <= MinimalAmount;
+        public bool NeedToRefill => Quantity <= (MinQuantity);
         public bool IsFull => Quantity== MaxQuantity;
 
-        public int MinimalAmount = 1;
 
         public static List<Product> ListOfProductsType1_500 = new List<Product>
         {
@@ -26,7 +26,7 @@ namespace MagazynierApp.Shared
             new Product(){Name = "Środki Ochrony pracy",Quantity = 70, MaxQuantity = 100, ImageUrl = "img/products/srodki-ochrony-pracy.jpg"},
             new Product(){Name = "Artykuły Ścierne i tnące",Quantity = 50,MaxQuantity = 200,ImageUrl = "img/products/artykuly-scierne-i-tnace.jpg"},
             new Product(){Name = "Chemia Techniczna",Quantity = 90,MaxQuantity = 150,ImageUrl = "img/products/chemia-techniczna.jpg"},
-            new Product(){Name = "Artykuły biurowe",MinimalAmount = 5,Quantity = 3, MaxQuantity = 50,ImageUrl = "img/products/artykuly-biurowe.jpg"},
+            new Product(){Name = "Artykuły biurowe",MinQuantity = 5,Quantity = 3, MaxQuantity = 50,ImageUrl = "img/products/artykuly-biurowe.jpg"},
         };
         public static List<Product> ListOfProductsTypeFull_500 = new List<Product>
         {
