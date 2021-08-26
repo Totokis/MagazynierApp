fba998888a1657a4c05c15242cf615bf04ffc533
Totokis <pawelj_janusz@wp.pl>
2021-08-26 11:14:11 +0200

    Added singleton of NotificationManager

--- MagazynierApp/Server/Startup.cs
+++ MagazynierApp/Server/Startup.cs
@@ -29,6 +29,7 @@ namespace MagazynierApp.Server
 
             services.Configure<VapidConfiguration>(Configuration.GetSection("VAPID"));
             services.AddControllers();
+            services.AddSingleton<INotificationManager,MachineNotificationManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
