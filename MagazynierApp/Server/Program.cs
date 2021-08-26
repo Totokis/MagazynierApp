ff798c9d39f2c2ec7b8bc7d18a82e80bb4c158e7
Totokis <pawelj_janusz@wp.pl>
2021-08-26 11:13:48 +0200

    Added monitoring service

--- MagazynierApp/Server/Program.cs
+++ MagazynierApp/Server/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -21,6 +22,8 @@ namespace MagazynierApp.Server
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                }).ConfigureServices(services => {
+                    services.AddHostedService<MachineMonitorService>();
                 });
     }
 }
