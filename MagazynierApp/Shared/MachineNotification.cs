7d85df229271a3fca48f352f6194a320067b53fc
Totokis <pawelj_janusz@wp.pl>
2021-08-26 09:28:30 +0200

    Properties for notification

--- MagazynierApp/Shared/MachineNotification.cs
+++ MagazynierApp/Shared/MachineNotification.cs
@@ -1,7 +1,21 @@
+using System;
+
 namespace MagazynierApp.Shared
 {
     public class MachineNotification
     {
+        public string MachineName { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int ValueNow { get; set; }
         
+        public int MaxValue { get; set; }
+
+        public string DistributionPoint { get; set; }
+
+        public DateTime AlertTime { get; set; }
+
+        public string Message { get; set; }
     }
 }
